75d7a12178d83ba37c57bdc505de308cf8b80396
MattScuteri <mttscuteri@gmail.com>
2018-01-12 01:50:52 -0500

    Storing user location in variable for use in ajax function...not complete yet

--- assets/js/yelp.js
+++ assets/js/yelp.js
@@ -2,16 +2,23 @@ $(document).ready(function() {
 	// Enter location - receive restuarant recommendations based on user input
 	// See phone number, website, ratings
 	// Integrate with Google maps using latitude/longitude inputs
+	// ONLY places to eat, limit results to 10, within 1 mile of userLocation
 
-	const userLocation = 
+
+	$('#user-input').submit(function(event) {
+		let userLocation = $('#add-btn').val().trim();
+		event.preventDefault();
+	})
+
 	const queryURL = "https://api.yelp.com/v3/businesses/search?location=" + userLocation;
 	const proxyUrl = 'https://shielded-hamlet-43668.herokuapp.com/';
+	
+	console.log(userLocation);	
 
 	$.ajax({
 		url: proxyUrl + queryURL,
 		headers: {
-			authorization: 'Bearer ' + "OZscrgovCzS7QRiOySG6zhu4i9R9LMeQLYN-EtOXvZ8WdyiYup_NGlDDPq-50il_VHt1nd3QPmN0Oq8xW7JkvGuiucxkFmzqJ2Bmh6G7BghJIPIi0CBy3UxsZwJYWnYx"
-		}
+			authorization: 'Bearer ' + //insert Yelp api
 	})
 
 	.done(response => {
