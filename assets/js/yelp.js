be852e6c826ff59ab993fff3537c8335d56f88f0
MattScuteri <mttscuteri@gmail.com>
2018-01-13 12:00:59 -0500

    Fixed restaurant results return issue

--- assets/js/yelp.js
+++ assets/js/yelp.js
@@ -1,24 +1,23 @@
-$(document).ready(function() {
 	// Enter location - receive restuarant recommendations based on user input
 	// See phone number, website, ratings
 	// Integrate with Google maps using latitude/longitude inputs
 	// ONLY places to eat, limit results to 10, within 1 mile of userLocation
 
 
-	let userLocation =  $('#add-btn').on('click', function(event) {
+	let userLocation;
+
+	$('#add-btn').on('click', function(event) {
 		event.preventDefault();		
-		$('#user-input').val().trim();
-	});
+		let userLocation = $('#user-input').val().trim();
+		console.log(userLocation);
 
-	const queryURL = "https://api.yelp.com/v3/businesses/search?location=" + userLocation + "&limit=10&radius=1610&categories=restaurants,all";
-	const proxyUrl = 'https://shielded-hamlet-43668.herokuapp.com/';
-	
-	console.log(userLocation);
-	console.log(queryURL);	
+		const queryURL = "https://api.yelp.com/v3/businesses/search?location=" + userLocation + "&limit=10&radius=1610&term=food&open_now=true";
+		const proxyUrl = 'https://shielded-hamlet-43668.herokuapp.com/';	
+		$('#content-results').empty();
 
-	$.ajax({
-		url: proxyUrl + queryURL,
-		headers: {
+		$.ajax({
+			url: proxyUrl + queryURL,
+			headers: {
 			authorization: 'Bearer ' + "OZscrgovCzS7QRiOySG6zhu4i9R9LMeQLYN-EtOXvZ8WdyiYup_NGlDDPq-50il_VHt1nd3QPmN0Oq8xW7JkvGuiucxkFmzqJ2Bmh6G7BghJIPIi0CBy3UxsZwJYWnYx"
 		}
 	})
@@ -29,16 +28,16 @@ $(document).ready(function() {
 
 			displayMarkers(response); //Added by Asha, keep this to send yelp response to map.js file
 
-			for (let i = 0; i < 10; i++) {
+			for (let i = 0; i < results.length; i++) {
 				const restaurantName = results[i].name;
-				const restuarantDistance = results[i].distance;
-				const restuarantPhone = results[i].phone;
+				const restaurantDistance = results[i].distance;
+				const restaurantPhone = results[i].phone;
 				const restaurantRating = results[i].rating;
 				const restaurantImg = $('<img>');
 				restaurantImg.addClass('restaurant-img');
 				$('#content-results').append(restaurantName);
-				$('#content-results').append(restuarantDistance);
-				$('#content-results').append(restuarantPhone);
+				$('#content-results').append(restaurantDistance);
+				$('#content-results').append(restaurantPhone);
 				$('#content-results').append(restaurantRating);
 				$('#content-results').append(restaurantImg);
 			};
@@ -46,7 +45,6 @@ $(document).ready(function() {
 
 		.catch(error => {
 			console.error(error);
-		});
+		});	
 
-})	
-
+	});
