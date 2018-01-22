326803cf446516e23c8295aa9a3b62f24f6677e2
asha <ashaagm@gmail.com>
2018-01-22 09:19:03 -0500

    adding mobile responsive bootstrap classes

--- assets/js/weather.js
+++ assets/js/weather.js
@@ -9,8 +9,8 @@ function getWeather(){
 		success : function(parsed_json) {
 			let location = parsed_json['location']['city'];
 			let temp_f = parsed_json['current_observation']['temp_f'];
-			console.log("Current temperature in " + location + " is: " + temp_f);
-			console.log(parsed_json['current_observation']['icon']);
+			// console.log("Current temperature in " + location + " is: " + temp_f);
+			// console.log(parsed_json['current_observation']['icon']);
 			let weather = "<span class='weather'><img src='" + parsed_json['current_observation']['icon_url'] + "'><br>" + temp_f + " °f</span>"
 			$("#div-weather").html(weather);
 		}
