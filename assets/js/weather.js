4ee488bfdee2d75dbe95facdc8d46a54c2f90347
asha <ashaagm@gmail.com>
2018-01-25 10:50:08 -0500

    fixes broken weather api

--- assets/js/weather.js
+++ assets/js/weather.js
@@ -1,13 +1,31 @@
 function getWeather(){
 
 	let inputAddress = localStorage.getItem("input-address");
-	let queryURL = "https://api.wunderground.com/api/8df60d93e424fab6/geolookup/conditions/q/" + inputAddress + ".json";
+	
+	inputAddress = inputAddress.split(" ");
+	let inputLength = inputAddress.length;
+	let queryURL = "";
+	// http://api.wunderground.com/api/YOUR_API_KEY/conditions/q/THE_DESIRED_STATE/THE_DESIRED_CITY.json
+	
+	if(inputLength > 2){
+		queryURL = "https://api.wunderground.com/api/8df60d93e424fab6/conditions/q/" + inputAddress[2]  + "/" + inputAddress[0] + " "
+		+ inputAddress[1] + ".json";
+	}else if(inputLength == 2){
+		queryURL = "https://api.wunderground.com/api/8df60d93e424fab6/conditions/q/" + inputAddress[1] + "/"
+		+ inputAddress[0] + ".json";
+	}else {
+		queryURL = "https://api.wunderground.com/api/8df60d93e424fab6/conditions/q/" 
+		+ inputAddress + ".json";
+	}
+
+	console.log("weather", encodeURI(queryURL));
 
 	$.ajax({
-		url : queryURL,				
+		url : encodeURI(queryURL),				
 		dataType : "jsonp",
 		success : function(parsed_json) {
-			let location = parsed_json['location']['city'];
+			console.log(parsed_json);
+			// let location = parsed_json['location']['city'];
 			let temp_f = parsed_json['current_observation']['temp_f'];
 			// console.log("Current temperature in " + location + " is: " + temp_f);
 			// console.log(parsed_json['current_observation']['icon']);
